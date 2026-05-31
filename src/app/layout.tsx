33f479c7832af51d020dfa77a028c78a3c6425a5
Psikuvit <thegengestar@gmail.com>
2026-05-31 21:30:42 +0100

    Feature: Add Vercel Analytics integration to layout component and update package dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { AuthGate } from '@/components/auth-gate'
 import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'BetterBlog',
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         <AuthGate>{children}</AuthGate>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
